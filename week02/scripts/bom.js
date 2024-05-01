54e71291e36b297927628dd5731d165a189409f7
yenfharm <yenfhael.arm@gmail.com>
2024-05-01 12:56:52 -0400

    changes

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+const myItem = input.value;
+input.value = '';
+  
+const listItem = document.createElement('li');
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+  
+listItem.append(listText);
+listText.textContent = myItem;
+listItem.append(listBtn);
+listBtn.textContent = 'X';
+listBtn.style.color = 'red';
+list.append(listItem);
+  
+listBtn.addEventListener('click', () => {
+list.remove(listItem);
+});
+  
+input.focus();
+});
