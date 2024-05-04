608e0483c9b46d55735a17f74adbd12798fec00c
yenfharm <yenfhael.arm@gmail.com>
2024-05-03 20:05:22 -0400

    changes

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,8 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
 
   // Update the last modified date
-  const lastModified = document.lastModified;
-  document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
+const lastModified = document.lastModified;
+document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
 
 
 const hamButton = document.querySelector('#menu');
