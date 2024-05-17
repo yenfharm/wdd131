62130beb172ba66e3bcc02eae9f9d58a8e447e3d
yenfharm <yenfhael.arm@gmail.com>
2024-05-17 15:40:23 -0400

    changes

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -1,9 +1,7 @@
 document.getElementById("currentyear").innerHTML = new Date().getFullYear();
-
   // Update the last modified date
 const lastModified = document.lastModified;
 document.getElementById("lastModified").innerHTML = `Last modified on: ${lastModified}`;
-
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
@@ -12,7 +10,6 @@ hamButton.addEventListener('click', () => {
     navigation.classList.toggle('open');
     hamButton.classList.toggle('open');
 });
-
 const temples = [
   {
       templeName: "Aba Nigeria",
@@ -86,9 +83,6 @@ const temples = [
   imageUrl:
   "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
 },
-  
-  
-  // Add more temple objects here...
 ];
 const templeCards = document.getElementById('temple-cards');
 
@@ -120,33 +114,3 @@ temples.forEach(temple => {
 
   templeCards.appendChild(card);
 });
-// createTempleCard();
-
-// function createTempleCard() {
-//   temples.forEach(temple => {
-//     let card = document.createElement("section");
-//     let name = document.createElement("h3");
-//     let location = document.createElement("p");
-//     let dedication = document.createElement("p");
-//     let area = document.createElement("p");
-//     let img = document.createElement("img");
-
-//     name.textContent = temple.templeName;
-//     location.innerHTML = `<span class="label">Location:</span> ${temple.location}`;
-//     dedication.innerHTML = `<span class="label">Dedicated:</span> ${temple.dedicated}`;
-//     area.innerHTML = `<span class="label">Size:</span> ${temple.area} sq ft`;
-
-//     img.setAttribute("src", temple.imageUrl);
-//     img.setAttribute("alt", `${temple.templeName} temple`);
-//     img.setAttribute("loading", "lazy");
-
-//     card.appendChild(name);
-//     card.appendChild(location);
-//     card.appendChild(dedication);
-//     card.appendChild(area);
-//     card.appendChild(img);
-
-//     document.querySelector("temple-cards").appendChild(card);
-
-//   });
-// }
