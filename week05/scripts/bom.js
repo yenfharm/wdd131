0eb5faf2acce5f60494232f78b6b966b2598811e
yenfharm <yenfhael.arm@gmail.com>
2024-05-25 10:22:18 -0400

    new

---
+++ week05/scripts/bom.js
@@ -0,0 +1,27 @@
+const list = document.querySelector('ul');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+const myItem = input.value;
+input.value = '';
+
+const listItem = document.createElement('li');
+const listText = document.createElement('span');
+const listBtn = document.createElement('button');
+
+listItem.append(listText);
+listText.textContent = myItem;
+listItem.append(listBtn);
+listBtn.textContent = 'X';
+listBtn.style.color = 'red';
+list.append(listItem);
+
+listBtn.addEventListener('click', () => {
+list.removeChild(listItem);
+});
+
+input.focus();
+});
+
+
+let chaptersArray = getChapterList();
